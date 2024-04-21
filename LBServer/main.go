cc314c21f32b4d4a372a1d9ab0ddb8b2594eda0d
Vexrina <vexrina.wlw@gmail.com>
2024-04-21 14:27:08 +0300

    probably done

--- LBServer/main.go
+++ LBServer/main.go
@@ -5,60 +5,151 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
 
 type EnvironmentMessage struct {
-	ServerLoads        []float64 `json:"server_loads"`
-	RequestQueueLength int       `json:"request_queue_length"`
-	ResponseTime       int       `json:"response_time"`
+	ServerLoads        []float64   `json:"server_loads"`
+	RequestQueueLength int         `json:"request_queue_length"`
+	ResponseTime       float64     `json:"response_time"`
+	RequestBody        RequestBody `json:"request_body"`
 }
 
+type RequestBody struct {
+	Prompt string `json:"prompt"`
+	Video  bool   `json:"video"`
+}
+
+type Request struct {
+	Body RequestBody
+	Resp chan Response
+}
+
+type Response struct {
+	Body []byte
+	Err  error
+}
+
+var maxLenQueue = 50
+var requestQueue = make(chan Request, maxLenQueue)
+
+var responseTime []float64
+
+var rlResponses = make(chan string)
+
 // Функция отправки сообщения в кафку
-func sendEnvironment(){
+func sendEnvironment(request Request) {
 	writer := kafka.NewWriter(kafka.WriterConfig{
 		Brokers: []string{"localhost:9092"},
-		Topic: "environment_topic",
+		Topic:   "environment_topic",
 	})
 
-	envMessage := getEnvironmentState()
+	envMessage := getEnvironmentState(request)
 
-	jsonMessage, err:= json.Marshal(envMessage)
+	jsonMessage, err := json.Marshal(envMessage)
 	if err != nil {
 		log.Printf("Error encoding message to json: %v", err)
 		return
 	}
 
 	err = writer.WriteMessages(context.Background(), kafka.Message{
-		Key: []byte(""),
+		Key:   []byte(""),
 		Value: jsonMessage,
 	})
-	if err!=nil{
+	if err != nil {
 		log.Printf("Error send message to Kafka: %v", err)
 		return
 	}
-	
+
 	writer.Close()
 }
 
+func readRLResponse() {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   []string{"localhost:9092"},
+		Topic:     "rl_response",
+		Partition: 0,
+		MinBytes:  10e3, // 10KB
+		MaxBytes:  10e6, // 10MB
+	})
 
-func getEnvironmentState() EnvironmentMessage {
-	return nil
+	defer reader.Close()
+
+	for {
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Printf("Error reading message from Kafka: %v", err)
+			continue
+		}
+
+		message := string(msg.Value)
+		rlResponses <- message
+	}
+}
+
+func average(slice []float64) float64 {
+	sum := 0.0
+	for _, num := range slice {
+		sum += num
+	}
+	return sum / float64(len(slice))
+}
+
+func sendGetRequest(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func getEnvironmentState(request Request) EnvironmentMessage {
+	serverURL := "localhost:"
+	ports := []string{"8081", "8082", "8083"}
+	var serverLoads []float64
+	for _, port := range ports {
+		out, err := sendGetRequest(serverURL + port + "/load")
+		if err != nil {
+			fmt.Printf("Ошибка при отправке запроса на сервер: %v\n", err)
+			continue
+		}
+
+		var load float64
+		_, err = fmt.Sscanf(string(out), "Server: %s, GPUload: %f", &port, &load)
+		if err != nil {
+			fmt.Printf("Ошибка при парсинге ответа от сервера: %v\n", err)
+			continue
+		}
+
+		serverLoads = append(serverLoads, load)
+	}
+	meanResponseTime := average(responseTime)
+	requestQueueLength := len(requestQueue) / 50
+
+	return EnvironmentMessage{serverLoads, requestQueueLength, meanResponseTime, request.Body}
 }
 
 // Функция отправки запроса на сервер
-func sendRequest(url string, requestBody []byte) ([]byte, error) {
+func sendPostRequest(url string, requestBody []byte) ([]byte, error) {
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(requestBody))
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
@@ -68,66 +159,95 @@ func sendRequest(url string, requestBody []byte) ([]byte, error) {
 
 // Обработчик запросов
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Проверяем, что метод запроса - POST
 	if r.Method != http.MethodPost {
 		http.Error(w, "Метод запроса должен быть POST", http.StatusMethodNotAllowed)
 		return
 	}
 
 	// Получаем тело запроса
-	var requestBody map[string]interface{}
+	var requestBody RequestBody
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Ошибка при декодировании тела запроса: %v", err), http.StatusBadRequest)
 		return
 	}
 
-	// Пример: отправляем тело запроса на RL нейронную сеть для анализа и получения результата
-	// Вместо этого используйте ваш код обращения к нейронной сети RL
-	rlResponse := map[string]interface{}{
-		"server": "server1", // Пример: результат от RL нейронной сети
-	}
-
-	// Определяем сервер для перенаправления запроса на основе ответа от RL нейронной сети
-	serverToRedirect := rlResponse["server"].(string)
-
-	// Определяем URL сервера на основе ответа от RL нейронной сети
-	serverURL := "localhost:"
-	port := "null"
-	switch serverToRedirect {
-	case "server1":
-		port = "8081"
-	case "server2":
-		port = "8082"
-	case "server3":
-		port = "8083"
+	select {
+	case requestQueue <- Request{Body: requestBody, Resp: make(chan Response)}:
+		// Запрос добавлен в очередь
 	default:
-		http.Error(w, "Недопустимый сервер", http.StatusInternalServerError)
+		http.Error(w, "Очередь запросов переполнена", http.StatusServiceUnavailable)
 		return
 	}
 
-	// Преобразуем тело запроса в []byte
-	requestBodyBytes, err := json.Marshal(requestBody)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка при кодировании тела запроса: %v", err), http.StatusInternalServerError)
-		return
-	}
+	req := <-requestQueue
+	defer close(req.Resp)
 
-	// Перенаправляем запрос на выбранный сервер
-	responseBody, err := sendRequest(serverURL+port, requestBodyBytes)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Ошибка при отправке запроса на сервер: %v", err), http.StatusInternalServerError)
+	// Отправляем запрос в очередь
+	requestQueue <- req
+
+	// Получаем ответ из очереди
+	resp := <-req.Resp
+
+	if resp.Err != nil {
+		http.Error(w, fmt.Sprintf("Ошибка при отправке запроса на сервер: %v", resp.Err), http.StatusInternalServerError)
 		return
 	}
 
 	// Возвращаем ответ от выбранного сервера
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(responseBody)
+	w.Write(resp.Body)
 }
 
-func main() {	
-	
+func worker() {
+	for req := range requestQueue {
+		start := time.Now()
+		sendEnvironment(req)
+		rlResponse := <-rlResponses
+
+		// Определяем сервер для перенаправления запроса на основе ответа от RL нейронной сети
+		serverToRedirect := rlResponse
+
+		// Определяем URL сервера на основе ответа от RL нейронной сети
+		serverURL := "localhost:"
+		port := "null"
+		switch serverToRedirect {
+		case "server1":
+			port = "8081"
+		case "server2":
+			port = "8082"
+		case "server3":
+			port = "8083"
+		default:
+			req.Resp <- Response{Err: fmt.Errorf("недопустимый сервер")}
+			continue
+		}
+
+		// Преобразуем тело запроса в []byte
+		requestBodyBytes, err := json.Marshal(req.Body)
+		if err != nil {
+			req.Resp <- Response{Err: fmt.Errorf("ошибка при кодировании тела запроса: %v", err)}
+			continue
+		}
+
+		// Перенаправляем запрос на выбранный сервер
+		responseBody, err := sendPostRequest(serverURL+port, requestBodyBytes)
+		if err != nil {
+			req.Resp <- Response{Err: fmt.Errorf("ошибка при отправке запроса на сервер: %v", err)}
+			continue
+		}
+
+		// Отправляем ответ в канал
+		req.Resp <- Response{Body: responseBody}
+		responseTime = append(responseTime, float64(time.Since(start)))
+	}
+}
+
+func main() {
+	go worker()
+	go readRLResponse()
+
 	port := ":8080"
 
 	http.HandleFunc("/", handler)
